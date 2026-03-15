2b3367df8f6d2b198fc384a2670924b615a1e266
Ramon Valencia <199168436+RamonVH18@users.noreply.github.com>
2026-03-14 17:25:47 -0700

    Creacion Modulo EnfermeriaDTO
    En este modulo se agregaran DTOS y mappers los cual servira para no amarrar a los servicios entre ellos.

---
+++ ProyectoEnfermeria/EnfermeriaDTOs/src/main/java/com/mycompany/enfermeriamapping/EnfermeriaMapping.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.enfermeriamapping;
+
+/**
+ *
+ * @author Ramon Valencia 
+ */
+public class EnfermeriaMapping {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
