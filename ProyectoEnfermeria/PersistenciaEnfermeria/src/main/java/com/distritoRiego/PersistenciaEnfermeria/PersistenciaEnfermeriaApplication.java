8cd2852d94b27cf503474a454e11b94e80e63b0b
Ramon Valencia <199168436+RamonVH18@users.noreply.github.com>
2026-03-14 17:13:20 -0700

    Creacion Persistencia
    Se creo el proyecto de persistencia en el que iran nuestros objetos DAO y conexion con la base de datos.

---
+++ ProyectoEnfermeria/PersistenciaEnfermeria/src/main/java/com/distritoRiego/PersistenciaEnfermeria/PersistenciaEnfermeriaApplication.java
@@ -0,0 +1,13 @@
+package com.distritoRiego.PersistenciaEnfermeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaEnfermeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenciaEnfermeriaApplication.class, args);
+	}
+
+}
---
+++ ProyectoEnfermeria/PersistenciaEnfermeria/src/test/java/com/distritoRiego/PersistenciaEnfermeria/PersistenciaEnfermeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distritoRiego.PersistenciaEnfermeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenciaEnfermeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
