c11212a890a74214553ef760d636672019f3b715
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2026-03-14 18:18:35 -0700

    Update ServicioCitas
    Tuve que volver a incluir la clase ServicioCitasApplication para que el proyecto pudiera compilarse. También modifiqué un poco la organización del proyecto.

---
+++ ProyectoEnfermeria/ServicioCitas/src/main/java/servicios/ServicioCitas.java
@@ -0,0 +1,13 @@
+package servicios;
+
+import interfaces.IServicioCitas;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Leonardo Flores Leyva
+ */
+@Service
+public class ServicioCitas implements IServicioCitas{
+    
+}
