6a64f874aa4d3a2ce5dcfc46e1c7b6e3682a11a9
Ramon Valencia <199168436+RamonVH18@users.noreply.github.com>
2026-03-16 14:39:17 -0700

    Correccion Leonardiada

--- ProyectoEnfermeria/ServicioCitas/src/main/java/servicios/ServicioCitas.java
+++ ProyectoEnfermeria/ServicioCitas/src/main/java/servicios/ServicioCitas.java
@@ -19,7 +19,7 @@ public class ServicioCitas implements IServicioCitas{
 
     @Override
     public CitaDTO crear(CitaDTO cita) throws CitasException {
-        
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
