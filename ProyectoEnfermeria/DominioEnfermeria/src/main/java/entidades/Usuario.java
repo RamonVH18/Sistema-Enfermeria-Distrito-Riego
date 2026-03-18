16d0037264c36acbcf17ed5a22e0beb2ecca0e5f
Isaac <161816557+isaacgro@users.noreply.github.com>
2026-03-17 21:57:16 -0700

    entidad Usuario
    creo que asi mero o nose

---
+++ ProyectoEnfermeria/DominioEnfermeria/src/main/java/entidades/Usuario.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author isaac
+ */
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Integer idUsuario;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "id_empleado", nullable = false, unique = true)
+    private Empleado empleado;
+    
+    public Usuario () {
+        
+    }
+
+    public Usuario( String email, String password, Empleado empleado) {
+        this.email = email;
+        this.password = password;
+        this.empleado = empleado;
+    }
+    
+    
+
+}
