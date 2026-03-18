7fd11c3bf59a949d72f1adaad1e07b9d5ebe4cbe
Isaac <161816557+isaacgro@users.noreply.github.com>
2026-03-17 21:58:34 -0700

    XD LO HICE CTRL S
    push

--- ProyectoEnfermeria/DominioEnfermeria/src/main/java/entidades/Usuario.java
+++ ProyectoEnfermeria/DominioEnfermeria/src/main/java/entidades/Usuario.java
@@ -45,6 +45,38 @@ public class Usuario {
         this.password = password;
         this.empleado = empleado;
     }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
     
     
 
